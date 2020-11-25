f6f772190dd9541d0b061ca4f86bceead1e10ee0
Samantha Halliburton <samlhalliburton@gmail.com>
2020-11-26 00:17:05 +0100

    Correct Checkstyle Issues

--- src/main/java/Decorator/FarmerGrowing.java
+++ src/main/java/Decorator/FarmerGrowing.java
@@ -2,16 +2,16 @@ package main.java.Decorator;
 
 public class FarmerGrowing extends FarmerDecorator {
 
-  public FarmerGrowing(Farmer farmer) {
-    super(farmer);
-  }
+    public FarmerGrowing(Farmer farmer) {
+        super(farmer);
+    }
 
-  public String decorate() {
-    return super.decorate() + decorateWithGrowing();
-  }
+    public String decorate() {
+        return super.decorate() + decorateWithGrowing();
+    }
 
-  private String decorateWithGrowing() {
-    return " and has an affinity for growing.";
-  }
+    private String decorateWithGrowing() {
+        return " and has an affinity for growing.";
+    }
 
 }
--- src/main/java/Factory/FarmFactory.java
+++ src/main/java/Factory/FarmFactory.java
@@ -9,7 +9,7 @@
 
 package main.java.Factory;
 
-/*
+/**
  * Base factory class for farms.
  */
 public abstract class FarmFactory {
@@ -18,6 +18,10 @@ public abstract class FarmFactory {
     int days = 1;
     int nights = 0;
 
+    /**
+     * constructFarm()
+     * Creates a farm.
+     */
     public void constructFarm() {
 
         Farm newFarm = createNewFarm();
@@ -26,28 +30,45 @@ public abstract class FarmFactory {
 
     public abstract Farm createNewFarm();
 
+    /**
+     * printCurrentDay()
+     * Prints out the current day.
+     */
     public void printCurrentDay() {
         System.out.println("The current day is: Day " + days);
     }
 
+    /**
+     * nextDay()
+     * Sets the next day.
+     */
     public void nextDay() {
         if (nights == days) {
             days += 1;
             System.out.println("Day " + days + ": It is now daytime.");
-        } else return;
+        } else {
+            return;
+        }
     }
 
+    /**
+     * nextNight()
+     * Sets the next night.
+     */
     public void nextNight() {
         if ((days - nights) == 1) {
             nights += 1;
             System.out.println("Night " + nights + ": It is now nighttime.");
-        } else if ((days - nights) != 1) return;
+        } else if ((days - nights) != 1) {
+            return;
+        }
     }
 
-    public void setTotalCycles() {
-        this.totalCycles = 0;
-    }
-
+    /**
+     * getTotalCycles()
+     * Returns the total number of completed days (both daytime and nighttime have occurred).
+     * @return int totalCycles
+     */
     public int getTotalCycles() {
         if (nights == days) {
             totalCycles = nights;
